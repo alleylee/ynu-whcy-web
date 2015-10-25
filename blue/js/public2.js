ba739d4f77ccd1c5fed18085b09ea5570fd543ad
alleylee <alleylee@sina.cn>
2015-10-25 22:15:20 +0800

    update

---
+++ blue/js/public2.js
@@ -0,0 +1,18 @@
+﻿$(function(){
+	//导航栏
+	navFuc();	
+})
+
+//导航栏
+function navFuc(){
+	if($("#mainNav")){
+		$("#mainNav li").hover(
+		function(){
+			$(this).addClass("active");
+		},
+		function(){
+			$(this).removeClass("active");
+		}
+		)	
+	}
+}
